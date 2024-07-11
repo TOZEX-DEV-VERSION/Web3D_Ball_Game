6bb3c60a2a37c757ddc6da14eded498a1ec6e508
bizarro <hello@lhbzr.com>
2024-07-11 16:05:54 +0900

    Add controllers

---
+++ src/stores/useAudio.js
@@ -0,0 +1,20 @@
+
+
+import { create } from "zustand";
+import { subscribeWithSelector } from "zustand/middleware";
+import { getLocalStorage, setLocalStorage } from "./utils";
+
+const useAudio = create(
+  subscribeWithSelector((set) => ({
+    audio: getLocalStorage("audio") ? true: false,
+    toggleAudio: () => {
+      set((state) => {
+        const newAudioState = !!state.audio;
+        setLocalStorage("audio", newAudioState);
+        return { audio: newAudioState };
+      });
+    },
+  }))
+);
+
+export default useAudio;
